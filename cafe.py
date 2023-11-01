fbd44c5cea7e60fe0e894504954219b511dcff96
Kate Hooker <katehooker88@gmail.com>
2023-11-01 22:43:19 +1300

    menu formatted, tip adding to total

--- cafe.py
+++ cafe.py
@@ -10,9 +10,25 @@ def convert_tuple(menu_item):
     return f"{menu_item['Product']}: ${menu_item['Price']:.2f}"
 
 def print_menu():
-    print('**********\nCafe Menu\n**********')
+    print('**********\nCafe Menu\n**********\n')
     for item in menu:
         menu_item_str = convert_tuple(item) 
         print(menu_item_str)
+    print('\n**********\nTo order a drink, simply type the item name and hit enter.\n')
 
-print_menu()
+order_total = 0
+
+menu_greeting = input("Welcome to the Python Cafe!\n\n\nWould you like to see our menu? (Y/N) ")
+if menu_greeting.lower() == 'y':
+    print_menu()
+else:
+    print("Don't waste my time! Next, please.")
+    tip_or_leave = input("\n\nTo leave the line, type 'Go'\nTo tip the barista $5 and see the menu, type 'Stay': ")
+    if tip_or_leave.lower() == 'go':
+        print("Thank you for your time.")
+    elif tip_or_leave.lower() == 'stay':
+        print_menu()
+        order_total += 5
+        print(f"Your order total is ${order_total:.2f}")
+    else:
+        print("Invalid choice. Goodbye!")
